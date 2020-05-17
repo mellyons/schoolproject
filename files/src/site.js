8f636970da87f79bacfef8dcedd555ba347e1d0b
mellyons <39783057+mellyons@users.noreply.github.com>
2020-05-17 10:22:01 -0500

    Add files via upload

---
+++ files/src/site.js
@@ -0,0 +1,22 @@
+//Bank Links
+$(document).ready(function() {
+	$("#headLinks").hide();
+	$("#ql") .click(function() {
+		$("#headLinks").slideToggle();
+	});
+
+//Enhance radio buttons
+$(function() {
+    $("input").checkboxradio();
+	});	
+
+//Sliding Images
+$(document) .ready(function(){
+	var i = 0;
+	slideImage(i);
+	
+	var lastI = $(".cards") .length -1;
+	function slideImage
+		$(".cards").eq(i).show();
+	)
+});
